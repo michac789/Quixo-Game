f92c57a0c6f3c27ba2bab4955a06789fe2eb4f67
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2021-10-14 15:26:40 +0800

    edited check_move function

--- quixo.py
+++ quixo.py
@@ -5,54 +5,52 @@ def main():
     #prompt for the dimension form the user
     #IMPROVE LATER ON > check the data type, make sure it's integer, add limit
     n = int(input("Choose dimention: "))
-    board = [0 for i in range(n*n)]
+    board = [i for i in range(int(n*n))]
+
+    display_board(board)
+    apply_move(board, 1, 14, 'L')
+    display_board(board)
+
+    apply_move(board, 2, 20, 'R')
+    display_board(board)
+
+"""     board = [0 for i in range(n*n)]
     print(board)
     display_board(board)
-    check_move(board, 1, 1, 'T')
+    returnval = check_move(board, 1, 1, 'T')
+    print(returnval)
+
+    turn = 0
+    while(True):
+        turn = turn + 1
+
+        
+        #input from the user
+        input_index = input("Enter index: ")
+        push_from = input("Enter push direction: ")
+        
+        if check_move(board, turn, input_index, push_from) == True:
+            break """
+
+    #display_board(board)
+    
+        #
 
 
 def check_move(board, turn, index, push_from):
     dimension = int(math.sqrt(len(board)))
-    # implement your function here
-    # if player 1 turn, cannot move pieces where player 2 is there
-    if turn == 1:
-        if board[index] == 2:
-            return False
-    # if player 2 turn, cannot move pieces where player 1 is there
-    if turn == 2:
-        if board[index] == 1:
-            return False
-    # if in the middle, cannont be moved
-    if ((index + 1) % dimension != 1 and (index + 1) % dimension != 0 and index >= dimension and index < dimension * (dimension - 1)):
+    if (turn == 1 and board[index] == 2) or (turn == 2 and board[index] == 1):
         return False
-    # top side > L, R, B 
-    if (((index + 1) % dimension != 1 and (index + 1) % dimension != 0) and index < dimension):
-        if push_from == 'T':
-            return False
-        else:
-            return True
-    # down side > L, R, T
-    if (((index + 1) % dimension != 1 or (index + 1) % dimension != 0) and index >= dimension * (dimension - 1)):
-        if push_from == 'B':
-            return False
-        else:
-            return True
-    # left side > B, T, R
-    if ((index + 1) % dimension != 1 and (index + 1) > dimension and (index + 1) < dimension * (dimension - 1)):
-        if push_from == 'L':
-            return False
-        else:
-            return True
-    # right side > B, T, L
-    if ((index + 1) % dimension != 0 and (index + 1) > dimension and (index + 1) < dimension * (dimension - 1)):
-        if push_from == 'R':
-            return False
-        else:
-            return True
+    if [(index + 1) % dimension == 1 and push_from == 'L'] or [(index + 1) % dimension == 0 and push_from == 'R']:
+        return False
+    if [(index < dimension) and push_from == 'T'] or [(index >= dimension * (dimension - 1)) and push_from == 'B']:
+        return False
+    if (index + 1) % dimension != 1 and (index + 1) % dimension != 0 and index >= dimension and index < dimension * (dimension - 1):
+        return False
+    return True
 
 def apply_move(board, turn, index, push_from):
-    # implement your function here
-    return board[:]
+    pass
 
 def check_victory(board, who_played):
     # implement your function here
@@ -68,7 +66,6 @@ def display_board(board):
         for j in range(dimension):
             print(board[i * dimension + j], end="     ")
         print("")
-    pass
 
 def menu():
     # implement your function here
